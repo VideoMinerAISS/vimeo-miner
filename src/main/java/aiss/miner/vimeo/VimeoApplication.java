f9df4118079bce7c1da0db55f70f0a132514333f
Pepe <coritochico@gmail.com>
2024-04-25 16:28:42 +0200

    Spring Initialization

---
+++ src/main/java/aiss/miner/vimeo/VimeoApplication.java
@@ -0,0 +1,13 @@
+package aiss.miner.vimeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeoApplication.class, args);
+	}
+
+}
