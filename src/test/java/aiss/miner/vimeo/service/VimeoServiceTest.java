efa3ecdf4ba5a935891ea2bced464895c2b77262
Pepe <coritochico@gmail.com>
2024-04-27 11:33:26 +0200

    Changed to Maven

--- src/test/java/aiss/miner/vimeo/service/VimeoServiceTest.java
+++ src/test/java/aiss/miner/vimeo/service/VimeoServiceTest.java
@@ -5,7 +5,7 @@ import aiss.miner.vimeo.model.vimeo.VimeoChannel;
 import aiss.miner.vimeo.model.vimeo.VimeoComment;
 import aiss.miner.vimeo.model.vimeo.VimeoVideo;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
